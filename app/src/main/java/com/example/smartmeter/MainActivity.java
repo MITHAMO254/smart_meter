ba1fe3c1592ad3d039ceb48dc4fd88b8bd9ae502
amshel <amshelhack3r@gmail.com>
2019-08-19 12:37:23 +0000

    Parsed json data

--- app/src/main/java/com/example/smartmeter/MainActivity.java
+++ app/src/main/java/com/example/smartmeter/MainActivity.java
@@ -2,12 +2,94 @@ package com.example.smartmeter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = "mutall";
+    private JSONArray jsonArray;
+    private List<Reading> readings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        readings = new ArrayList<>();
+        getMeterReadings();
+    }
+
+    public void getMeterReadings(){
+        //creating a request queue
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url = "http://afriot.net/afriot_consumer_api/afriotAPI.php";
+
+        StringRequest readings = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                convertStringToJson(response);
+                try {
+                    JSONObject object = jsonArray.getJSONObject(1);
+                    JSONArray readings = object.getJSONArray("modem_usage");
+
+                    for (int i = 0; i<readings.length(); i++){
+                        JSONObject individual = readings.getJSONObject(i);
+                        String prev_value = individual.getString("initial_reading");
+                        String current_value = individual.getString("final_reading");
+                        String date = individual.getString("flow_timestamp");
+                        String usage = individual.getString("consumption_estimate");
+
+                        Reading reading = new Reading(prev_value, current_value,date, usage);
+                        readings.put(reading);
+                    }
+                }catch (JSONException e){
+                    Log.e(TAG, e.getMessage());
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.e(TAG, error.getMessage());
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("auth_key", "0203ndfwu506bxfuopva5ybdge");
+                params.put("id_request", "13");
+                params.put("modem_hex", "72E1C5");
+                params.put("from_date", "2019-08-06");
+                params.put("to_date", "2019-08-15");
+
+                return params;
+            }
+        };
+        queue.add(readings);
+
+    }
+    private void convertStringToJson(String data){
+        try {
+            jsonArray = new JSONArray(data);
+        }catch (JSONException e){
+            Log.e(TAG, e.getMessage());
+        }
     }
 }
---
+++ app/src/main/java/com/example/smartmeter/Reading.java
@@ -0,0 +1,5 @@
+package com.example.smartmeter;
+
+public class Reading {
+    public Reading(String previous, String current, String date, String consumption){}
+}
