6ccdf7e8f9d9fefa647d8706f196030e3545b873
amshel <amshelhack3r@gmail.com>
2019-09-02 12:25:30 +0000

    Class for adding onclick and date dialog to an edittext

---
+++ app/src/main/java/com/example/smartmeter/CustomEditPicker.java
@@ -0,0 +1,44 @@
+package com.example.smartmeter;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+public class CustomEditPicker implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
+    EditText editText;
+    Context context;
+    String date;
+    public CustomEditPicker(Context context, int id){
+        Activity activity = (Activity)context;
+        editText  = activity.findViewById(id);
+        this.context = context;
+        editText.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        int new_month = month + 1;
+        editText.setText(year+"-"+new_month+"-"+dayOfMonth);
+        date = year+"-"+new_month+"-"+dayOfMonth;
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Calendar calendar = Calendar.getInstance();
+
+        DatePickerDialog dialog = new DatePickerDialog(context, this, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH));
+        dialog.show();
+    }
+
+
+    public String getDate(){
+        return date;
+    }
+}
