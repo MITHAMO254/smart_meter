9ba82198a28e94528831d7d4d9b17eb3fb621bd9
solo <solomonwachira212@gmail.com>
2019-09-03 17:04:54 +0300

    commented code

--- app/src/main/java/com/example/smartmeter/CustomEditPicker.java
+++ app/src/main/java/com/example/smartmeter/CustomEditPicker.java
@@ -10,20 +10,33 @@ import android.widget.EditText;
 import java.util.Calendar;
 
 public class CustomEditPicker implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
+    //
+    // define edittext of type EditText.
     EditText editText;
     Context context;
     String date;
+
     public CustomEditPicker(Context context, int id){
+        //
+        // create an activity and assing it to context.
         Activity activity = (Activity)context;
+        //
+        //asign date_value to the editetxt.
         editText  = activity.findViewById(id);
+
         this.context = context;
+        //
+        //set onclick event listener to the EditText.
         editText.setOnClickListener(this);
     }
 
-
+    //
+    // Override the onDateSet method.
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
         int new_month = month + 1;
+        //
+        //Display the year, month and day on the edit text.
         editText.setText(year+"-"+new_month+"-"+dayOfMonth);
         date = year+"-"+new_month+"-"+dayOfMonth;
 
@@ -31,8 +44,11 @@ public class CustomEditPicker implements View.OnClickListener, DatePickerDialog.
 
     @Override
     public void onClick(View v) {
+       //
+       //get an instance of calender and store it in calendar.
         Calendar calendar = Calendar.getInstance();
-
+        //
+        //instantiate DatePickerDialog
         DatePickerDialog dialog = new DatePickerDialog(context, this, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH));
         dialog.show();
     }
